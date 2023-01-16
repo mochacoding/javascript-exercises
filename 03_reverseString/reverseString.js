9ba3b1a7da79f600051b3678aeb492b6a84e07e5
Alan Hernandez <alan.hernandez19@gmail.com>
2023-01-16 06:38:29 -0600

    finish exercise 1 through 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	let arrString = string.split('');
+	arrString = arrString.reverse();
+	arrString = arrString.join('');
+	return arrString;
 };
 
 // Do not edit below this line
