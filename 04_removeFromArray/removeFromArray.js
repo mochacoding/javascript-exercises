b2f2037b89f429fd52e10b172b3c5b3becc809df
Alan Hernandez <alan.hernandez19@gmail.com>
2023-01-17 09:15:25 -0600

    solve 4 by implementing rest param and loop' ''
    git status
    '

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...more) {
+	const myArray = arr;
+	for (let i = 0; i < more.length; i++) {
+		const index = myArray.indexOf(more[i]);
+		if (index == -1) {
+			continue;
+		}
+		myArray.splice(index, 1);
+	}
+	return myArray;
 };
+// var removeFromArray = function (...args) {
+// 	const array = args[0];
+// 	return array.filter((val) => !args.includes(val));
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
